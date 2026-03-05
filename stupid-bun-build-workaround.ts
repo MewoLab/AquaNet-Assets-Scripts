58c83475853194c32bcaaf851c6e7dbe42e4dffb
Raymond <101374892+raymonable@users.noreply.github.com>
2026-03-04 23:22:36 -0500

    chore: remove unnecessary bullshit

--- stupid-bun-build-workaround.ts
+++
@@ -1,5 +0,0 @@
-import { build } from "vite"
-build().then(() => process.exit())
-// For some reason, Vite doesn't exit upon finishing its build when run in Bun.
-// I have no clue why. If someone knows why, please correct it.
-// -May
